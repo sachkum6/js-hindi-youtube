da3d14b56ab90787880d22d11668989b5a2d976c
sachkum6 <91514487+sachkum6@users.noreply.github.com>
2026-02-10 22:09:36 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,39 @@
+
+// let score = 33
+// console.log(typeof score) // number
+
+// console.log(typeof (score)) //number - small n
+
+// let run = "33"
+// let valueInNumber = Number(run)
+// console.log(typeof valueInNumber) // number
+
+// let run1 = "33abc"
+// let valueInNumber1 = Number(run1)
+// console.log(typeof valueInNumber1) // number
+// console.log(valueInNumber1) //NaN - its a special type
+
+// let temp = null
+// let valueInTemp = Number(temp)
+// console.log(typeof valueInTemp) // number
+// console.log(valueInTemp) //0
+
+
+// let temp1 = undefined
+// let valueInTemp1 = Number(temp1)
+// console.log(typeof valueInTemp1) // number
+// console.log(valueInTemp1) //NaN
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn); // true
+
+// 1 => true; 0 => false
+//"" => false
+// "hitesh" => true
+
